438e97685c1623614dc67f114d44649215cfc725
rnail1989 <rnail1989@mail.ru>
2020-10-15 20:19:24 +0300

    Update Folmula

--- src/Deposit.java
+++ src/Deposit.java
@@ -1,12 +1,12 @@
 public class Deposit {
     double calculate(double summ, double persent, int month) {
-        if (month < 0) {
-            return 0;
+        if (month <= 0) {
+            return 0.0;
         }
         int coefficent = 12;
 
         double percentMonth = persent / coefficent;
-        double profit = ((summ*percentMonth)/month)*month;
+        double profit = ((summ * percentMonth) / month) * month;
 
         return profit;
     }
